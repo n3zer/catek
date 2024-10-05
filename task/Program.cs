fe06455fd68ff7b334a4a90c5b79f6c1c21b001d
n3z <kirill.mel07@gmail.com>
2024-10-05 11:36:03 +0500

    task 2

---
+++ task/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {  
+            Human n = new Human();
+            n.age = 10;
+            Human n2 = new Human();
+            n.age = 20;
+        }
+        
+    }
+}
+
---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// 1. *Расчёт скорости при свободном падении:*  
+//    Рассчитайте скорость тела при свободном падении через 
+//    время t по формуле: v = v_0 + g * t, где v_0 —
+//     начальная скорость, g = 9.8 метров в секунду в квадрате, 
+//     t — время падения. Выведите как скорость, так и путь,
+//      пройденный телом по формуле: s = v_0 * t + (1 / 2) * g * t^2.
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V_0, V;
+            double g = 9.8;
+            double t;
+
+            double s;
+
+            Console.Write("v_0");
+            Double.TryParse(Console.ReadLine(), out V_0);
+            Console.Write("t");
+            Double.TryParse(Console.ReadLine(), out t);
+            V = V_0+g*t;
+
+            s = V_0 * t + (1/2)*g*(t*t);
+
+            Console.WriteLine(s);
+
+
+        }
+    }
+}
